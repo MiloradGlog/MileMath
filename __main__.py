cefe35a415b0b87c49e97a0854ae93b542307aae
MiloradGlog <miloradst@hotmail.com>
2017-11-16 01:34:03 +0100

    boldovao ispis

--- __main__.py
+++ __main__.py
@@ -4,17 +4,29 @@ from interpreter.interpreter import Interpreter
 from interpreter.interpreter import NUMBER
 from interpreter.trie import Trie
 
-
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
 
 def main():
     root = Trie("ROOT", "ROOT")
     while True:
         try:
-            text = input('milemath>>>> ')
+            text = input('\033[1mmilemath: \033[0m')
         except (EOFError, KeyboardInterrupt):
             break
         if not text:
             continue
+        elif text == "EXIT":
+            print("ALOO")
         lexer = Lexer(text)
         parser = Parser(lexer)
         interpreter = Interpreter(parser, root)
